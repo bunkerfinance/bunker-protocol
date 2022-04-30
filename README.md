d0fce9c04fbed767307b149f7c661859cd3a5b73
Bunker Dev <hello@bunker.finance>
2022-04-29 18:29:16 -0700

    Compound fork with whitespace fixes

---
+++ README.md
@@ -0,0 +1,3 @@
+# swapdao-deploy
+
+Branched off [Compound 2.9](https://github.com/SwapDAO-dev/compound-protocol/commit/ae4388e780a8d596d97619d9704a931a2752c2bc).
---
+++ hardhat.config.ts
@@ -0,0 +1,35 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig } from "hardhat/types";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-solhint";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9",
+      },
+      {
+        version: "0.5.16",
+      }
+    ]
+  },
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      rinkeby: process.env.ETHERSCAN_API_KEY || "",
+    },
+  },
+};
+export default config;
